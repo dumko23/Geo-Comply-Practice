62cb87bb2ff89e66a1fbee240b42de3a96e39209
Dmytro_Parkhomenko <dmytro.parkhomenko@geocomply.com>
2021-12-30 11:53:14 +0200

    Finally, it works the proper way. Laravel page opens

--- composer-laravel/script/script.sh
+++ composer-laravel/script/script.sh
@@ -1,5 +1,13 @@
 composer create-project laravel/laravel example-app
 
-cd example-app
+cd /var/www/example-app/storage
+chmod o+w logs
 
-php artisan serve
+cd /var/www/example-app/storage
+chmod o+w framework/sessions
+
+cd /var/www/example-app/storage
+chmod o+w framework/views
+
+
+
