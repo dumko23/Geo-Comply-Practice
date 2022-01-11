4ca22dd168b0f22e4597acf595e31c3f847710a1
Dmytro_Parkhomenko <dmytro.parkhomenko@geocomply.com>
2022-01-11 22:02:30 +0200

    L9-T1-3 completed

---
+++ Compose-task-1/source-file/Lecture-3/Lesson-9_Cookie/Task-1.php
@@ -0,0 +1,35 @@
+<?php
+date_default_timezone_set('Europe/Kiev');
+
+if (!isset($_COOKIE['user']) && !isset($_COOKIE['count'])) {
+    setcookie(
+        'user',
+        date('d/m/Y H:i:s ', ),
+        time() + 10 * 60 * 60
+    );
+    setcookie(
+        'count',
+        $count = 1,
+        time() + 10 * 60 * 60
+    );
+    echo "Hello, newbie!";
+} else {
+
+    $lastVisited = $_COOKIE['user'];
+    $count = $_COOKIE['count'];
+
+    setcookie(
+        'user',
+        date('d/m/Y H:i:s', ),
+        time() + 10 * 60 * 60
+    );
+    setcookie(
+        'count',
+        $count = $_COOKIE['count'] + 1,
+        time() + 10 * 60 * 60
+    );
+    echo "Welcome back, master!" . '<br>';
+    echo 'Last visit: ' . $lastVisited . '<br>';
+    echo 'Times visited before: ' . $count;
+}
+
